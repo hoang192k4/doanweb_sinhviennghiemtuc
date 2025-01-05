3bb784b00f8b4e690092f62be0fcf890889707e4
Hoang <nguyenngochoang192k4@gmail.com>
2025-01-05 12:07:38 +0700

    hoàng-Hiển thị thông tin dịch vụ cửa hàng-v2

--- resources/views/User/pages/index.blade.php
+++ resources/views/User/pages/index.blade.php
@@ -383,37 +383,6 @@
             </button>
         </div>
     </section>
-
     {{-- Hiển thị thông tin dịch vụ bán hàng, vận chuyển --}}
-    <section class="container_css service">
-        <div class="service_items">
-            <div class="service_item">
-                <div class="item_icon"><i class="fas fa-truck"></i></div>
-                <div class="item_text">
-                    <h6>MIỄN PHÍ VẬN CHUYỂN</h6>
-                    <p>100% trách nhiệm về vận chuyển</p>
-                </div>
-            </div>
-            <div class="service_item">
-                <div class="item_icon"><i class="fas fa-money-bill-alt"></i></div>
-                <div class="item_text">
-                    <h6>THANH TOÁN KHI NHẬN ĐƯỢC HÀNG</h6>
-                </div>
-            </div>
-            <div class="service_item">
-                <div class="item_icon"><i class="fas fa-undo-alt"></i></div>
-                <div class="item_text">
-                    <h6>ĐỔI TRẢ TRONG 7 NGÀY</h6>
-                    <p>1 đổi 1 nếu lỗi nằm ở shop</p>
-                </div>
-            </div>
-            <div class="service_item">
-                <div class="item_icon"><i class="far fa-clock"></i></div>
-                <div class="item_text">
-                    <h6>MỞ CỬA CẢ TUẦN</h6>
-                    <p>7:00 giờ sáng đến 20:00 tối</p>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('user.partials.service')
 @endsection
