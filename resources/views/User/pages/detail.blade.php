970e892ee722ad3ab1fee808ca085ece91fa3b36
Hoang <nguyenngochoang192k4@gmail.com>
2025-01-13 16:08:27 +0700

    hoàng-update-logo

--- resources/views/User/pages/detail.blade.php
+++ resources/views/User/pages/detail.blade.php
@@ -231,7 +231,7 @@
 
     <!-- Sản phẩm tương tự -->
     <section class="container_css product_best_seller">
-        <h4>ĐIỆN THOẠI NỔI BẬT NHẤT</h4>
+        <h4>SẢN PHẨM TƯƠNG TỰ</h4>
         <div id="carouselExampleInterval" class="carousel slide carousel-dark" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
